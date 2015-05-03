22bd553004f754d0808b7816f4c9985da1b616bf
Tim Steen <tls8963@rit.edu>
2015-05-03 12:32:59 -0400

    Magnetic Collectables and methane in cluster unpinned.

--- Assets/Scripts/Behaviours/CollectableBehaviour.cs
+++ Assets/Scripts/Behaviours/CollectableBehaviour.cs
@@ -5,6 +5,7 @@ public class CollectableBehaviour : MonoBehaviour {
 	public bool m_respawn;
 	private bool canBeam = true;
 	private EventPublisher eventPublisher;
+	GameObject player;
 
 	//need to have Start() so we can disable this.
 	void Start()
@@ -14,6 +15,7 @@ public class CollectableBehaviour : MonoBehaviour {
 		} else { 
 			Debug.Log ("No level game object in scene: " + Application.loadedLevelName);
 		}
+		player = GameObject.Find ("Player");	
 	}
 
 	void OnCollisionEnter(Collision collision) {
@@ -23,6 +25,26 @@ public class CollectableBehaviour : MonoBehaviour {
 			gameObject.SetActive(false);
 			if(m_respawn)
 				Invoke("Reactivate", 1);
+		}
+	}
+
+	void Update()
+	{
+		Rigidbody rigidBody = gameObject.GetComponent<Rigidbody> ();
+		if (rigidBody != null) {
+			Vector3 toPlayer = player.transform.position - transform.position;
+			//cannot equal 0
+			float radius = 6;
+			float min = 0.45f, max = 1.0f;
+			float range = max - min;
+			float percentage = (radius - toPlayer.magnitude) / radius;
+			if(percentage > 0)
+			{
+				percentage = min + (percentage*range);
+				float multiplier = 100;
+				toPlayer.Normalize();
+				rigidBody.AddForce(toPlayer * percentage * multiplier);
+			}
 		}
 	}
 
